bcc711bd09a9ffff5ee00e35105a0dd17a9cf981
David Torrejano <davitorrejano@gmail.com>
2023-05-28 22:40:31 -0500

    Justificación del patrón

--- src/mediator/Main.java
+++ src/mediator/Main.java
@@ -17,6 +17,9 @@ public class Main {
         user1.sendMessage("Hola mis amigos");
         user2.sendMessage("Hola Martín, ¿cómo vas?");
         user3.sendMessage("Ey Martí, tiempo sin saber de ti.");
+        /*
+        * Escogí este patrón porque en principio me pareció el más difícil de todos, y por otro lado
+        * debido a todas las formas en las que se podría implementar dicho patrón*/
 
     }
 }
